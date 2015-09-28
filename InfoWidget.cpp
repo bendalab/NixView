d241456d5e12a2459d0e4d0998bfe486d2992bcc
Lorand Madai <lorand.madai@uni-tuebingen.de>
2015-09-28 14:15:16 +0200

    added metadata info

--- InfoWidget.cpp
+++ InfoWidget.cpp
@@ -10,7 +10,7 @@ InfoWidget::InfoWidget(QWidget *parent) :
     is_active = false;
 }
 
-void InfoWidget::update_info_widget(std::string id, std::string type, std::string name, boost::optional<std::basic_string<char>> description)
+void InfoWidget::update_info_widget(std::string id, std::string type, std::string name, boost::optional<std::basic_string<char>> description, nix::Section meta_section)
 {
     if (!is_active)
     {
@@ -32,9 +32,11 @@ void InfoWidget::update_info_widget(std::string id, std::string type, std::strin
         ss << "-";
     QString info_string = QString::fromStdString(ss.str());
     ui->info_text_edit->setText(info_string);
+
+    update_meta_info(meta_section);
 }
 
-void InfoWidget::update_info_widget(std::string id, std::string name, boost::optional<std::basic_string<char>> description)
+void InfoWidget::update_info_widget(std::string id, std::string name, boost::optional<std::basic_string<char>> description, nix::Section meta_section)
 {
     if (!is_active)
     {
@@ -55,9 +57,58 @@ void InfoWidget::update_info_widget(std::string id, std::string name, boost::opt
         ss << "-";
     QString info_string = QString::fromStdString(ss.str());
     ui->info_text_edit->setText(info_string);
+
+    update_meta_info(meta_section);
+}
+
+void InfoWidget::update_meta_info(nix::Section meta_section)
+{
+    ui->treeWidget->clear();
+
+    if(meta_section)
+    {
+        QTreeWidgetItem* branch = new QTreeWidgetItem(ui->treeWidget, QStringList(QString::fromStdString(meta_section.name())));
+
+        for (nix::Section s : meta_section.sections())
+        {
+            QTreeWidgetItem* tree_item = new QTreeWidgetItem(branch, QStringList(QString::fromStdString(s.name())));
+            add_children_to_item(tree_item, s);
+        }
+
+        ui->treeWidget->expandItem(branch);
+        ui->treeWidget->resizeColumnToContents(0);
+    }
+}
+
+void InfoWidget::add_children_to_item(QTreeWidgetItem* item, nix::Section section)
+{
+    for  (auto s : section.sections())
+    {
+        QTreeWidgetItem* child_item = new QTreeWidgetItem(item, QStringList(QString::fromStdString(s.name())));
+        add_children_to_item(child_item, s);
+    }
+
+    for (nix::Property p : section.properties())
+    {
+        QTreeWidgetItem* child_item = new QTreeWidgetItem(item, QStringList(QString::fromStdString(p.name())));
+        child_item->setText(1, QString::fromStdString("Metadata"));
+        child_item->setText(2, QString::fromStdString(nix::data_type_to_string(p.dataType())));
+    }
+}
+
+void InfoWidget::resize_to_content(QModelIndex qml)
+{
+    for (int c = 0; c<ui->treeWidget->columnCount();c++)
+        ui->treeWidget->resizeColumnToContents(c);
 }
 
 InfoWidget::~InfoWidget()
 {
     delete ui;
+}
+
+//getter
+const QTreeWidget* InfoWidget::get_tree_widget()
+{
+    return ui->treeWidget;
 }
--- InfoWidget.hpp
+++ InfoWidget.hpp
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include <QtGui>
 #include <boost/optional.hpp>
+#include <nix.hpp>
 
 namespace Ui {
 class InfoWidget;
@@ -20,14 +21,22 @@ public:
     bool is_active;
 
 public slots:
-    void update_info_widget(std::string, std::string, std::string, boost::optional<std::basic_string<char>>);
-    void update_info_widget(std::string, std::string, boost::optional<std::basic_string<char>>);
+    void update_info_widget(std::string, std::string, std::string, boost::optional<std::basic_string<char>>, nix::Section);
+    void update_info_widget(std::string, std::string, boost::optional<std::basic_string<char>>, nix::Section);
+
+    void resize_to_content(QModelIndex);
 
 signals:
     void add_info_widget();
 
 private:
     Ui::InfoWidget *ui;
+
+    void update_meta_info(nix::Section);
+    void add_children_to_item(QTreeWidgetItem*, nix::Section);
+
+public:
+    const QTreeWidget* get_tree_widget();
 
 };
 
