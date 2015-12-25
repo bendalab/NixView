fb8dcf3cb9b697a752f7581853631e8493b3ee7d
Jan Grewe <jan.grewe@g-node.org>
2015-12-25 17:35:37 +0100

    remove ending semicolons to remove compile warnings

--- common/Common.hpp
+++ common/Common.hpp
@@ -1,10 +1,10 @@
 #include <nix.hpp>
 
-Q_DECLARE_METATYPE(nix::Block);
-Q_DECLARE_METATYPE(nix::Section);
-Q_DECLARE_METATYPE(nix::Property);
-Q_DECLARE_METATYPE(nix::DataArray);
-Q_DECLARE_METATYPE(nix::Tag);
-Q_DECLARE_METATYPE(nix::MultiTag);
-Q_DECLARE_METATYPE(nix::Source);
+Q_DECLARE_METATYPE(nix::Block)
+Q_DECLARE_METATYPE(nix::Section)
+Q_DECLARE_METATYPE(nix::Property)
+Q_DECLARE_METATYPE(nix::DataArray)
+Q_DECLARE_METATYPE(nix::Tag)
+Q_DECLARE_METATYPE(nix::MultiTag)
+Q_DECLARE_METATYPE(nix::Source)
 
