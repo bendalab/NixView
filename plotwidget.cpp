5de4480a181786d665079ade17dc2651fab48746
Jan Grewe <jan.grewe@g-node.org>
2015-12-30 19:08:15 +0100

    [PlotWidget] add check methods, set_entity

--- plotwidget.cpp
+++ plotwidget.cpp
@@ -1,14 +1,40 @@
 #include "plotwidget.h"
 #include "ui_plotwidget.h"
+#include "lineplotter.h"
+#include "common/Common.hpp"
 
 PlotWidget::PlotWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::PlotWidget)
 {
     ui->setupUi(this);
+    std::cerr << "PlotWidget" << std::endl;
 }
 
 PlotWidget::~PlotWidget()
 {
     delete ui;
+}
+
+
+bool PlotWidget::can_draw() const {
+    return this->item.canConvert<nix::DataArray>() | this->item.canConvert<nix::MultiTag>() | this->item.canConvert<nix::Tag>();
+}
+void PlotWidget::setEntity(QVariant var) {
+    this->item = var;
+    select_plotter();
+    if (can_draw()) {
+        draw();
+    }
+}
+
+
+bool PlotWidget::check_plottable_dtype(nix::DataType dtype) const {
+    bool plottable = true;
+    plottable = plottable && dtype != nix::DataType::Bool;
+    plottable = plottable && dtype != nix::DataType::String;
+    plottable = plottable && dtype != nix::DataType::Char;
+    plottable = plottable && dtype != nix::DataType::Opaque;
+    plottable = plottable && dtype != nix::DataType::Nothing;
+    return plottable;
 }
