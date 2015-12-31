434a736f374b3f4524302318ffc2e8c3eda76940
Jan Grewe <jan.grewe@g-node.org>
2015-12-31 16:23:42 +0100

    [LinePlotter] activate user interactions

--- plotter/lineplotter.cpp
+++ plotter/lineplotter.cpp
@@ -1,11 +1,22 @@
 #include "lineplotter.h"
 #include "ui_lineplotter.h"
+#include <QMenu>
 
 LinePlotter::LinePlotter(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::LinePlotter)
-{
+    QWidget(parent), ui(new Ui::LinePlotter) {
     ui->setupUi(this);
+    // connect slot that ties some axis selections together (especially opposite axes):
+    connect(ui->plot, SIGNAL(selectionChangedByUser()), this, SLOT(selection_changed()));
+    // connect slots that takes care that when an axis is selected, only that direction can be dragged and zoomed:
+    connect(ui->plot, SIGNAL(mousePress(QMouseEvent*)), this, SLOT(mouse_press()));
+    connect(ui->plot, SIGNAL(mouseWheel(QWheelEvent*)), this, SLOT(mouse_wheel()));
+    // make bottom and left axes transfer their ranges to top and right axes:
+    connect(ui->plot->xAxis, SIGNAL(rangeChanged(QCPRange)), ui->plot->xAxis2, SLOT(setRange(QCPRange)));
+    connect(ui->plot->yAxis, SIGNAL(rangeChanged(QCPRange)), ui->plot->yAxis2, SLOT(setRange(QCPRange)));
+    // setup policy and connect slot for context menu popup:
+    ui->plot->setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(ui->plot, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(context_menu_request(QPoint)));
+    ui->plot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables | QCP::iSelectAxes);
 }
 
 
@@ -89,6 +100,216 @@ void LinePlotter::add_segments(const QVector<double> &positions, const QVector<d
     }
 }
 
+
 QCustomPlot* LinePlotter::get_plot() {
     return ui->plot;
+}
+
+
+void LinePlotter::selection_changed() {
+    std::cerr << "selected " << std::endl;
+    // make top and bottom axes be selected synchronously, and handle axis and tick labels as one selectable object:
+    if (ui->plot->xAxis->selectedParts().testFlag(QCPAxis::spAxis) || ui->plot->xAxis->selectedParts().testFlag(QCPAxis::spTickLabels) ||
+            ui->plot->xAxis->selectedParts().testFlag(QCPAxis::spAxisLabel) || ui->plot->xAxis2->selectedParts().testFlag(QCPAxis::spAxis) ||
+            ui->plot->xAxis2->selectedParts().testFlag(QCPAxis::spTickLabels) || ui->plot->xAxis2->selectedParts().testFlag(QCPAxis::spAxisLabel))
+    {
+        ui->plot->xAxis2->setSelectedParts(QCPAxis::spAxis|QCPAxis::spTickLabels|QCPAxis::spAxisLabel);
+        ui->plot->xAxis->setSelectedParts(QCPAxis::spAxis|QCPAxis::spTickLabels|QCPAxis::spAxisLabel);
+    }
+    // make left and right axes be selected synchronously, and handle axis and tick labels as one selectable object:
+    if (ui->plot->yAxis->selectedParts().testFlag(QCPAxis::spAxis) || ui->plot->yAxis->selectedParts().testFlag(QCPAxis::spTickLabels) ||
+            ui->plot->yAxis->selectedParts().testFlag(QCPAxis::spAxisLabel) || ui->plot->yAxis2->selectedParts().testFlag(QCPAxis::spAxis) ||
+            ui->plot->yAxis2->selectedParts().testFlag(QCPAxis::spTickLabels) || ui->plot->yAxis2->selectedParts().testFlag(QCPAxis::spAxisLabel))
+    {
+        ui->plot->yAxis2->setSelectedParts(QCPAxis::spAxis|QCPAxis::spTickLabels|QCPAxis::spAxisLabel);
+        ui->plot->yAxis->setSelectedParts(QCPAxis::spAxis|QCPAxis::spTickLabels|QCPAxis::spAxisLabel);
+    }
+
+    // synchronize selection of graphs with selection of corresponding legend items:
+    for (int i=0; i<ui->plot->graphCount(); ++i) {
+        QCPGraph *graph = ui->plot->graph(i);
+        QCPPlottableLegendItem *item = ui->plot->legend->itemWithPlottable(graph);
+        if (item->selected() || graph->selected()) {
+            item->setSelected(true);
+            graph->setSelected(true);
+        }
+    }
+}
+
+
+void LinePlotter::mouse_press() {
+    std::cerr << "mouse press " << std::endl;
+    // if an axis is selected, only allow the direction of that axis to be dragged
+    // if no axis is selected, both directions may be dragged
+    if (ui->plot->xAxis->selectedParts().testFlag(QCPAxis::spAxis))
+        ui->plot->axisRect()->setRangeDrag(ui->plot->xAxis->orientation());
+    else if (ui->plot->yAxis->selectedParts().testFlag(QCPAxis::spAxis))
+        ui->plot->axisRect()->setRangeDrag(ui->plot->yAxis->orientation());
+    else
+        ui->plot->axisRect()->setRangeDrag(Qt::Horizontal|Qt::Vertical);
+}
+
+
+void LinePlotter::mouse_wheel() {
+    std::cerr << "mouse wheel " << std::endl;
+    // if an axis is selected, only allow the direction of that axis to be zoomed
+    // if no axis is selected, both directions may be zoomed
+    if (ui->plot->xAxis->selectedParts().testFlag(QCPAxis::spAxis))
+        ui->plot->axisRect()->setRangeZoom(ui->plot->xAxis->orientation());
+    else if (ui->plot->yAxis->selectedParts().testFlag(QCPAxis::spAxis))
+        ui->plot->axisRect()->setRangeZoom(ui->plot->yAxis->orientation());
+    else
+        ui->plot->axisRect()->setRangeZoom(Qt::Horizontal|Qt::Vertical);
+}
+
+
+void LinePlotter::context_menu_request(QPoint pos) {
+    std::cerr << "context menu" << std::endl;
+    QMenu *menu = new QMenu(this);
+    menu->setAttribute(Qt::WA_DeleteOnClose);
+
+    QAction *legend_action = menu->addAction("Show legend", this, SLOT(show_legend()));
+    legend_action->setCheckable(true);
+    legend_action->setChecked(ui->plot->legend->visible());
+    menu->addAction("Clear selection", this, SLOT(clear_selection()));
+    if (ui->plot->selectedGraphs().size() > 0) {
+        menu->addAction("Remove selected graph", this, SLOT(remove_selected_graph()));
+        QMenu *line_style_menu = menu->addMenu("Line style");
+        line_style_menu->addAction("none", this, SLOT(set_pen_none()));
+        line_style_menu->addAction("solid", this, SLOT(set_pen_solid()));
+        line_style_menu->addAction("dashed", this, SLOT(set_pen_dashed()));
+        line_style_menu->addAction("dash-dotted", this, SLOT(set_pen_dashed()));
+        line_style_menu->addAction("dotted", this, SLOT(set_pen_dashed()));
+
+        QMenu *marker_menu = menu->addMenu("Marker");
+        marker_menu->addAction("none", this, SLOT(marker_none()));
+        marker_menu->addAction("circle", this, SLOT(marker_circle()));
+        marker_menu->addAction("cross", this, SLOT(marker_cross()));
+        marker_menu->addAction("dot", this, SLOT(marker_dot()));
+        marker_menu->addAction("diamond", this, SLOT(marker_diamond()));
+        marker_menu->addAction("plus", this, SLOT(marker_plus()));
+        marker_menu->addAction("Square", this, SLOT(marker_square()));
+    }
+    menu->popup(ui->plot->mapToGlobal(pos));
+}
+
+
+void LinePlotter::remove_selected_graph() {
+    if (ui->plot->selectedGraphs().size() > 0) {
+        ui->plot->removeGraph(ui->plot->selectedGraphs().first());
+        ui->plot->replot();
+    }
+}
+
+
+void LinePlotter::show_legend() {
+    ui->plot->legend->setVisible(!ui->plot->legend->visible());
+    ui->plot->replot();
+}
+
+
+void LinePlotter::clear_selection(){
+    ui->plot->deselectAll();
+    ui->plot->replot();
+}
+
+
+void LinePlotter::set_marker(QString marker){
+    QCPGraph *graph = ui->plot->selectedGraphs().first();
+    if (marker == "none") {
+        graph->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssNone, 0));
+    } else if (marker == "circle") {
+        graph->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, 10));
+    } else if (marker == "diamond") {
+        graph->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssDiamond, 10));
+    } else if (marker == "dot") {
+        graph->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssDot, 10));
+    } else if (marker == "cross") {
+        graph->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCross, 10));
+    } else if (marker == "square") {
+        graph->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssSquare, 10));
+    } else if (marker == "plus") {
+        graph->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssPlus, 10));
+    } else {
+        graph->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssNone, 10));
+    }
+    clear_selection();
+}
+
+
+void LinePlotter::marker_circle() {
+    set_marker("circle");
+}
+
+
+void LinePlotter::marker_cross() {
+    set_marker("cross");
+}
+
+
+void LinePlotter::marker_dot() {
+    set_marker("dot");
+}
+
+
+void LinePlotter::marker_diamond() {
+    set_marker("diamond");
+}
+
+void LinePlotter::marker_square() {
+    set_marker("square");
+}
+
+void LinePlotter::marker_none(){
+    set_marker("none");
+}
+
+void LinePlotter::marker_plus(){
+    set_marker("plus");
+}
+
+
+void LinePlotter::set_pen_style(QString style) {
+    QCPGraph *graph = ui->plot->selectedGraphs().first();
+    QPen pen = graph->pen();
+    if (style == "none"){
+        pen.setStyle(Qt::PenStyle::NoPen);
+    } else if (style == "solid") {
+        pen.setStyle(Qt::PenStyle::SolidLine);
+    } else if (style == "dashed") {
+        pen.setStyle(Qt::PenStyle::DashLine);
+    } else if (style == "dotted") {
+        pen.setStyle(Qt::PenStyle::DotLine);
+    } else if (style == "dashdot") {
+        pen.setStyle(Qt::PenStyle::DashDotLine);
+    } else {
+        pen.setStyle(Qt::PenStyle::SolidLine);
+    }
+    graph->setPen(pen);
+    clear_selection();
+}
+
+
+void LinePlotter::set_pen_none() {
+    set_pen_style("none");
+}
+
+
+void LinePlotter::set_pen_solid() {
+    set_pen_style("solid");
+}
+
+
+void LinePlotter::set_pen_dashed() {
+    set_pen_style("dashed");
+}
+
+
+void LinePlotter::set_pen_dotted() {
+    set_pen_style("dotted");
+}
+
+
+void LinePlotter::set_pen_dashdotted() {
+    set_pen_style("dashdot");
 }
--- plotter/lineplotter.h
+++ plotter/lineplotter.h
@@ -33,6 +33,29 @@ private:
     Ui::LinePlotter *ui;
     QCustomPlot* get_plot();
 
+public slots:
+    void selection_changed();
+    void mouse_wheel();
+    void mouse_press();
+    void show_legend();
+    void context_menu_request(QPoint pos);
+    void remove_selected_graph();
+    void clear_selection();
+    void set_pen_style(QString style);
+    void set_pen_none();
+    void set_pen_solid();
+    void set_pen_dashed();
+    void set_pen_dashdotted();
+    void set_pen_dotted();
+    void set_marker(QString marker);
+    void marker_none();
+    void marker_square();
+    void marker_diamond();
+    void marker_circle();
+    void marker_cross();
+    void marker_dot();
+    void marker_plus();
+
 };
 
 #endif // LINEPLOTTER_H
