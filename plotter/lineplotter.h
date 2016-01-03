2270e601a36a5041e2df95a4d5712b6bf003df30
Jan Grewe <jan.grewe@g-node.org>
2016-01-03 17:16:50 +0100

    [LinePlotter] add missing prototypes

--- plotter/lineplotter.h
+++ plotter/lineplotter.h
@@ -19,9 +19,11 @@ public:
 
     void add_line_plot(const QVector<double> &x_data, const QVector<double> &y_data, const QString &name);
 
+    void set_ylabel(const std::string &label);
     void set_ylabel(const QString &label);
 
     void set_xlabel(const QString &label);
+    void set_xlabel(const std::string &label);
 
     void add_events(const QVector<double> &x_data, const QVector<double> &y_data, const QString &name, bool y_scale);
 
