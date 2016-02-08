23ea967dbae71812b7e612ff51debe43c4d20a5c
Lorand Madai <lorand.madai@uni-tuebingen.de>
2016-02-08 16:42:58 +0100

    changed filter to display everything

--- filter/NixProxyModel.cpp
+++ filter/NixProxyModel.cpp
@@ -3,19 +3,20 @@
 NixProxyModel::NixProxyModel(QObject *parent)
     :QSortFilterProxyModel(parent)
 {
-    setFilterRegExp(QRegExp("*fish*"));
+    setFilterRegExp(QRegExp("Metadata"));
 }
 
 bool NixProxyModel::filterAcceptsRow(int source_row, const QModelIndex &source_parent) const
 {
-    NixDataModel *model = static_cast<NixDataModel*>(sourceModel());
-    for (int i = 0; i < model->num_columns; ++i)
-    {
-        QModelIndex index = model->index(source_row, i, source_parent);
-        if (model->data(index).toString().contains(filterRegExp()))
-            return true;
-    }
-    return false;
+//    NixDataModel *model = static_cast<NixDataModel*>(sourceModel());
+//    for (int i = 0; i < model->num_columns; ++i)
+//    {
+//        QModelIndex index = model->index(source_row, i, source_parent);
+//        if (model->data(index).toString().contains(filterRegExp()))
+//            return true;
+//    }
+//    return false;
+    return true;
 }
 
 
