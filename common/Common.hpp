c1947ae64fdd730e8aa62295a4d0957fd77cf148
Lorand Madai <lorand.madai@uni-tuebingen.de>
2016-02-29 11:37:40 +0100

    added nix Group

--- common/Common.hpp
+++ common/Common.hpp
@@ -1,6 +1,7 @@
 #include <nix.hpp>
 
 Q_DECLARE_METATYPE(nix::Block)
+Q_DECLARE_METATYPE(nix::Group)
 Q_DECLARE_METATYPE(nix::Section)
 Q_DECLARE_METATYPE(nix::Property)
 Q_DECLARE_METATYPE(nix::DataArray)
