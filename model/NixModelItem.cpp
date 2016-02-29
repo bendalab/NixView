18071f18321bc16a6dcc5211168f4080dfec8d14
Lorand Madai <lorand.madai@uni-tuebingen.de>
2016-02-29 12:20:00 +0100

    added getter

--- model/NixModelItem.cpp
+++ model/NixModelItem.cpp
@@ -4,17 +4,34 @@
 NixModelItem::NixModelItem()
     : QStandardItem()
 {
-
+    nix_entity_type = "";
 }
 
-NixModelItem::NixModelItem(const QString & text)
+NixModelItem::NixModelItem(const QString &text)
     : QStandardItem(text)
 {
-
+    nix_entity_type = "";
 }
 
-NixModelItem::NixModelItem(const QString & text, QVariant _nix_entity)
+NixModelItem::NixModelItem(const QString &text, QVariant _nix_entity)
     : QStandardItem(text)
 {
     nix_entity = _nix_entity;
+
+    if(nix_entity.canConvert<nix::Block>())
+        nix_entity_type = "Block";
+    else if(nix_entity.canConvert<nix::Group>())
+        nix_entity_type = "Group";
+    else if(nix_entity.canConvert<nix::DataArray>())
+        nix_entity_type = "DataArray";
+    else if(nix_entity.canConvert<nix::MultiTag>())
+        nix_entity_type = "MultiTag";
+    else if(nix_entity.canConvert<nix::Tag>())
+        nix_entity_type = "Tag";
+    else if(nix_entity.canConvert<nix::Section>())
+        nix_entity_type = "Section";
+    else if(nix_entity.canConvert<nix::Source>())
+        nix_entity_type = "Source";
+    else if(nix_entity.canConvert<nix::Property>())
+        nix_entity_type = "Property";
 }
--- model/NixModelItem.hpp
+++ model/NixModelItem.hpp
@@ -11,6 +11,11 @@ public:
     NixModelItem(const QString & text);
     NixModelItem(const QString & text, QVariant _nix_entity);
 
+    // getter
+    const std::string get_nix_qvariant_type() const { return nix_entity_type; }
+    template <typename T>
+    const T get_nix_entity() const { if(nix_entity.canConvert<T>()) return nix_entity.value<T>(); }
+
 private:
     QVariant nix_entity;
     std::string nix_entity_type;
