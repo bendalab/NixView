628d3abb493857cae56044d6da2311533384d5a6
Jan Grewe <jan.grewe@g-node.org>
2016-03-14 17:52:09 +0100

    remove pedantic extra ; warnings

--- common/Common.hpp
+++ common/Common.hpp
@@ -1,15 +1,15 @@
 #include <nix.hpp>
 
 // declare NIX entitites as QVariant types
-Q_DECLARE_METATYPE(nix::Block);
-Q_DECLARE_METATYPE(nix::Group);
-Q_DECLARE_METATYPE(nix::Section);
-Q_DECLARE_METATYPE(nix::Property);
-Q_DECLARE_METATYPE(nix::DataArray);
-Q_DECLARE_METATYPE(nix::Tag);
-Q_DECLARE_METATYPE(nix::MultiTag);
-Q_DECLARE_METATYPE(nix::Feature);
-Q_DECLARE_METATYPE(nix::Source);
+Q_DECLARE_METATYPE(nix::Block)
+Q_DECLARE_METATYPE(nix::Group)
+Q_DECLARE_METATYPE(nix::Section)
+Q_DECLARE_METATYPE(nix::Property)
+Q_DECLARE_METATYPE(nix::DataArray)
+Q_DECLARE_METATYPE(nix::Tag)
+Q_DECLARE_METATYPE(nix::MultiTag)
+Q_DECLARE_METATYPE(nix::Feature)
+Q_DECLARE_METATYPE(nix::Source)
 
 // define strings regarding NIX for consistent use
 #define NIX_STRING_BLOCK "Block"
