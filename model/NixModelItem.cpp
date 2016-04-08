4893412f203cdb17d5f3885b7f48f0a6a6400c12
Jan Grewe <jan.grewe@g-node.org>
2016-04-08 10:16:15 +0200

    deactivate tool tips

--- model/NixModelItem.cpp
+++ model/NixModelItem.cpp
@@ -24,73 +24,73 @@ NixModelItem::NixModelItem(const QString &text, QVariant _nix_entity)
     {
         nix_entity_type = NIX_STRING_BLOCK;
         entity_metadata = true;
-        nix::Block b = nix_entity.value<nix::Block>();
-        std::string tooltip = "Block: " + b.type() + " --- " + b.name();
-        setToolTip(QString::fromStdString(tooltip));
+//        nix::Block b = nix_entity.value<nix::Block>();
+//        std::string tooltip = "Block: " + b.type() + " --- " + b.name();
+//        setToolTip(QString::fromStdString(tooltip));
     }
     else if(nix_entity.canConvert<nix::Group>())
     {
         nix_entity_type = NIX_STRING_GROUP;
         entity_metadata = true;
-        nix::Group g = nix_entity.value<nix::Group>();
-        std::string tooltip = "Group: " + g.type() + " --- " + g.name();
-        setToolTip(QString::fromStdString(tooltip));
+//        nix::Group g = nix_entity.value<nix::Group>();
+//        std::string tooltip = "Group: " + g.type() + " --- " + g.name();
+//        setToolTip(QString::fromStdString(tooltip));
     }
     else if(nix_entity.canConvert<nix::DataArray>())
     {
         nix_entity_type = NIX_STRING_DATAARRAY;
         entity_metadata = true;
-        nix::DataArray da = nix_entity.value<nix::DataArray>();
-        std::string tooltip = "DataArray: " + da.type() + " --- " + da.name();
-        setToolTip(QString::fromStdString(tooltip));
+//        nix::DataArray da = nix_entity.value<nix::DataArray>();
+//        std::string tooltip = "DataArray: " + da.type() + " --- " + da.name();
+//        setToolTip(QString::fromStdString(tooltip));
     }
     else if(nix_entity.canConvert<nix::MultiTag>())
     {
         nix_entity_type = NIX_STRING_MULTITAG;
         entity_metadata = true;
-        nix::MultiTag mtag = nix_entity.value<nix::MultiTag>();
-        std::string tooltip = "MultiTag: " + mtag.type() + " --- " + mtag.name();
-        setToolTip(QString::fromStdString(tooltip));
+//        nix::MultiTag mtag = nix_entity.value<nix::MultiTag>();
+//        std::string tooltip = "MultiTag: " + mtag.type() + " --- " + mtag.name();
+//        setToolTip(QString::fromStdString(tooltip));
     }
     else if(nix_entity.canConvert<nix::Tag>())
     {
         nix_entity_type = NIX_STRING_TAG;
         entity_metadata = true;
-        nix::Tag t = nix_entity.value<nix::Tag>();
-        std::string tooltip = "Tag: " + t.type() + " --- " + t.name();
-        setToolTip(QString::fromStdString(tooltip));
+//        nix::Tag t = nix_entity.value<nix::Tag>();
+//        std::string tooltip = "Tag: " + t.type() + " --- " + t.name();
+//        setToolTip(QString::fromStdString(tooltip));
     }
     else if(nix_entity.canConvert<nix::Feature>())
     {
         nix_entity_type = NIX_STRING_FEATURE;
         entity_metadata = false;
-        nix::Feature f = nix_entity.value<nix::Feature>();
-        std::string tooltip = "Feature: " + f.data().type() + " --- " + f.data().type();
-        setToolTip(QString::fromStdString(tooltip));
+//        nix::Feature f = nix_entity.value<nix::Feature>();
+//        std::string tooltip = "Feature: " + f.data().type() + " --- " + f.data().type();
+//        setToolTip(QString::fromStdString(tooltip));
     }
     else if(nix_entity.canConvert<nix::Section>())
     {
         nix_entity_type = NIX_STRING_SECTION;
         entity_metadata = false;
-        nix::Section s = nix_entity.value<nix::Section>();
-        std::string tooltip = "Section: " + s.type() + " --- " + s.name();
-        setToolTip(QString::fromStdString(tooltip));
+//        nix::Section s = nix_entity.value<nix::Section>();
+//       std::string tooltip = "Section: " + s.type() + " --- " + s.name();
+//        setToolTip(QString::fromStdString(tooltip));
     }
     else if(nix_entity.canConvert<nix::Source>())
     {
         nix_entity_type = NIX_STRING_SOURCE;
         entity_metadata = true;
-        nix::Source s = nix_entity.value<nix::Source>();
-        std::string tooltip = "Source: " + s.type() + " --- " + s.name();
-        setToolTip(QString::fromStdString(tooltip));
+//        nix::Source s = nix_entity.value<nix::Source>();
+//        std::string tooltip = "Source: " + s.type() + " --- " + s.name();
+//        setToolTip(QString::fromStdString(tooltip));
     }
     else if(nix_entity.canConvert<nix::Property>())
     {
         nix_entity_type = NIX_STRING_PROPERTY;
         entity_metadata = false;
-        nix::Property p = nix_entity.value<nix::Property>();
-        std::string tooltip = "Property: " + p.name() + " --- " + (p.unit().is_initialized() ? *p.unit() : "");
-        setToolTip(QString::fromStdString(tooltip));
+//        nix::Property p = nix_entity.value<nix::Property>();
+//        std::string tooltip = "Property: " + p.name() + " --- " + (p.unit().is_initialized() ? *p.unit() : "");
+//        setToolTip(QString::fromStdString(tooltip));
     }
 }
 
