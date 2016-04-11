9c7ce03435cc3e9e43124a4fbda31615df97d22a
Jan Grewe <jan.grewe@g-node.org>
2016-04-11 14:36:15 +0200

    [plotwidget] delegate plotting to line- and category plotters

--- plotwidget.cpp
+++ plotwidget.cpp
@@ -65,47 +65,10 @@ void PlotWidget::process(const nix::DataArray &array) {
         ui->scrollAreaWidgetContents->layout()->addWidget(lp);
         lp->draw(array);
     } else if (Plotter::suggested_plotter(array) == PlotterType::Category) {
-
-    }
-
-
-    size_t dim_count = array.dimensionCount();
-    switch (dim_count) {
-    case 1:
-        if (array.getDimension(1).dimensionType() == nix::DimensionType::Sample ||
-                array.getDimension(1).dimensionType() == nix::DimensionType::Range) {
-            // delete_widgets_from_layout();
-            /*
-            LinePlotter *lp = new LinePlotter();
-            ui->scrollAreaWidgetContents->layout()->addWidget(lp);
-            this->plots.push_back(static_cast<Plotter*>(lp));
-            draw_1d(array);
-            */
-        } else if (array.getDimension(1).dimensionType() == nix::DimensionType::Set) {
-            CategoryPlotter *cp = new CategoryPlotter();
-            ui->scrollAreaWidgetContents->layout()->addWidget(cp);
-            this->plots.push_back(static_cast<Plotter*>(cp));
-            draw_1d(array);
-        }
-        break;
-    case 2:
-        if (array.getDimension(1).dimensionType() == nix::DimensionType::Sample ||
-                array.getDimension(1).dimensionType() == nix::DimensionType::Range) {
-            /*
-            ui->multiPlotCheckBox->setEnabled(true);
-            if (array.getDimension(2).dimensionType() == nix::DimensionType::Set) {
-                draw_multi_line(array);
-            } else {
-                // handle 2D image/heatmap plotting not supported, yet TODO
-            }
-            */
-        } else {
-            // handle 2 D set plotting  TODO
-        }
-        break;
-    default:
-        std::cerr << "Sorry, cannot plot data with more than 2d!" << std::endl;
-        break;
+        delete_widgets_from_layout();
+        CategoryPlotter *cp = new CategoryPlotter();
+        ui->scrollAreaWidgetContents->layout()->addWidget(cp);
+        cp->draw(array);
     }
 }
 
