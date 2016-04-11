5f40bda65ba932701a5bd6187a9405873a39a724
Jan Grewe <jan.grewe@g-node.org>
2016-04-11 14:27:44 +0200

    [categoryplotter] convenience funct for direct plotting of data array

--- plotter/categoryplotter.cpp
+++ plotter/categoryplotter.cpp
@@ -3,7 +3,8 @@
 
 CategoryPlotter::CategoryPlotter(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::CategoryPlotter)
+    ui(new Ui::CategoryPlotter),
+    cmap()
 {
     ui->setupUi(this);
 }
@@ -11,6 +12,120 @@ CategoryPlotter::CategoryPlotter(QWidget *parent) :
 CategoryPlotter::~CategoryPlotter()
 {
     delete ui;
+}
+
+
+void CategoryPlotter::draw(const nix::DataArray &array) {
+    if (array.dimensionCount() > 2) {
+        std::cerr << "CategoryPlotter::draw cannot draw 3D!" << std::endl;
+        return;
+    }
+    if (!Plotter::check_plottable_dtype(array)) {
+        std::cerr << "CategoryPlotter::draw cannot handle data type " << array.dataType() << std::endl;
+        return;
+    }
+    if (!check_dimensions(array)) {
+        std::cerr << "CategroyPlotter::draw cannot handle dimensionality of the data" << std::endl;
+        return;
+    }
+    if (array.dimensionCount() == 1) {
+        draw_1d(array);
+    } else {
+        draw_2d(array);
+    }
+}
+
+
+void CategoryPlotter::draw_1d(const nix::DataArray &array) {
+    nix::Dimension d = array.getDimension(1);
+    QVector<double> x_axis, y_axis;
+    QVector<QString> x_tick_labels;
+    data_array_to_qvector(array, x_axis, y_axis, x_tick_labels, 1);
+
+    QString y_label;
+    QVector<QString> ax_labels;
+    Plotter::data_array_ax_labels(array, y_label, ax_labels);
+    add_bar_plot(x_tick_labels, y_axis, QString::fromStdString(array.name()));
+    set_label(array.name());
+    set_ylabel((array.label() ? *array.label() : "") + (array.unit() ? (" [" + *array.unit() + "]") : ""));
+}
+
+
+void CategoryPlotter::draw_2d(const nix::DataArray &array) {
+    int best_dim = guess_best_xdim(array);
+    QVector<double> xaxis, yaxis;
+    QVector<QString> xlabels, ylabels;
+    get_data_array_axis(array, xaxis, xlabels, best_dim);
+    get_data_array_axis(array, yaxis, ylabels, 3-best_dim);
+    double ymin = 0.0;
+    double ymax = 0.0;
+    QCPBarsGroup *group = new QCPBarsGroup(ui->plot);
+    for (int i = 0; i < yaxis.size(); i++) {
+        QVector<double> data = get_data_line(array, i, best_dim);
+        QCPBars *bars = new QCPBars(ui->plot->xAxis, ui->plot->yAxis);
+        ui->plot->addPlottable(bars);
+        bars->setData(xaxis, data);
+        QColor c = cmap.next();
+        bars->setPen(c);
+        c.setAlpha(50);
+        bars->setBrush(c);
+        bars->setWidth(0.15);
+        bars->setBarsGroup(group);
+        bars->setName(ylabels[i]);
+        double mi = *std::min_element(std::begin(data), std::end(data));
+        double ma = *std::max_element(std::begin(data), std::end(data));
+        if (mi < ymin)
+            ymin = mi;
+        if (ma > ymax)
+            ymax= ma;
+    }
+    ui->plot->xAxis->setAutoTickStep(false);
+    ui->plot->xAxis->setAutoTickLabels(false);
+
+    ui->plot->xAxis->setTickStep(1);
+    ui->plot->xAxis->setSubTickCount(0);
+    ui->plot->xAxis->setTickLength(0, 4);
+    ui->plot->xAxis->setTickVector(xaxis);
+    ui->plot->xAxis->grid()->setVisible(true);
+    ui->plot->xAxis->setRange(-0.5, xaxis.size());
+    ui->plot->yAxis->setRange(1.5 * ymin, 1.5 * ymax);
+    ui->plot->legend->setVisible(true);
+    ui->plot->xAxis->setTickVectorLabels(xlabels);
+    ui->plot->xAxis->setTickLabelRotation(60);
+    set_label(array.name());
+    nix::Dimension dim = array.getDimension(3-best_dim);
+    std::string unit = "";
+    if (array.label())
+        set_ylabel(*array.label() + (array.unit() ? (" [" + *array.unit() + "]") : ""));
+}
+
+
+bool CategoryPlotter::check_dimensions(const nix::DataArray &array) const {
+    if (array.dimensionCount() == 0) {
+        return false;
+    }
+    if (array.dimensionCount() == 1) {
+        return true;
+    }
+    nix::DimensionType dt_1 = array.getDimension(1).dimensionType();
+    nix::DimensionType dt_2 = array.getDimension(2).dimensionType();
+    if (((dt_1 == nix::DimensionType::Sample || dt_1 == nix::DimensionType::Range) && dt_2 == nix::DimensionType::Set) ||
+            (dt_1 == nix::DimensionType::Set && (dt_2 == nix::DimensionType::Range || dt_2 == nix::DimensionType::Range))) {
+        std::cerr << "CategoryPlotter should draw 2D data with Range or SampleDimension? You serious? I can do this, but seems suboptimal..." << std::endl;
+        return true;
+    }
+    if (dt_1 == nix::DimensionType::Set && dt_2 == nix::DimensionType::Set) {
+        return true;
+    }
+    return false;
+}
+
+
+int CategoryPlotter::guess_best_xdim(const nix::DataArray &array) const {
+    nix::NDSize shape = array.dataExtent();
+    if (shape[0] > shape[1])
+        return 1;
+    return 2;
 }
 
 
@@ -65,7 +180,6 @@ void CategoryPlotter::add_bar_plot(QVector<QString> categories, QVector<double> 
     ui->plot->xAxis->setRange(-0.5, categories.size() - 0.5);
 
     ui->plot->yAxis->setPadding(5);
-    ui->plot->yAxis->setLabel("Power Consumption in\nKilowatts per Capita (2007)");
     ui->plot->yAxis->grid()->setSubGridVisible(true);
     double y_max = *std::max_element(std::begin(y_data), std::end(y_data));
     ui->plot->yAxis->setRange(0, 1.05 * y_max);
