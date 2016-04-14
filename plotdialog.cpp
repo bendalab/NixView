e75a48116d15fc409c4d2b0797e0ed8eb45536cc
Christian Kellner <christian@kellner.me>
2016-04-14 22:58:49 +0200

    [plotdialog] create close button in code
    Connect the signal to accept the dialog. This is so we can extend the
    button box in code with non-standard buttons.

--- plotdialog.cpp
+++ plotdialog.cpp
@@ -10,6 +10,11 @@ PlotDialog::PlotDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::PlotDialog) {
     ui->setupUi(this);
+
+    QPushButton *btnClose = new QPushButton(tr("&Close"));
+    ui->buttonBox->addButton(btnClose, QDialogButtonBox::AcceptRole);
+
+    connect(ui->buttonBox, &QDialogButtonBox::accepted, this, &PlotDialog::accept);
 }
 
 void PlotDialog::set_entity(QModelIndex qml) {
