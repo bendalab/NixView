18be6c8715cba732e5a2dfea7aa3c1c37c88d285
Jan Grewe <jan.grewe@g-node.org>
2016-04-17 23:33:57 +0200

    [tablemodel] horizontal header kind of working

--- model/nixarraytablemodel.cpp
+++ model/nixarraytablemodel.cpp
@@ -18,7 +18,37 @@ void NixArrayTableModel::set_source(const nix::DataArray &array) {
 
 QVariant NixArrayTableModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
+    if (role != Qt::DisplayRole) {
+        return QVariant();
+    }
     //orientation == Qt::Orientation::Horizontal
+    if (orientation == Qt::Orientation::Horizontal) {
+        if (shape.size() == 1) {
+            return QString::fromStdString((array.label() ? *array.label() : "") +
+                                          (array.unit() ? " [" + *array.unit() + "]" : ""));
+        } else {
+            if (array.getDimension(2).dimensionType() == nix::DimensionType::Set) {
+                nix::SetDimension dim = array.getDimension(2).asSetDimension();
+                std::vector<std::string> labels = dim.labels();
+                if (section > labels.size()) {
+                    return QString::fromStdString(nix::util::numToStr(section));
+;               }
+                return QString::fromStdString(labels[section]);
+            } else if (array.getDimension(2).dimensionType() == nix::DimensionType::Range) {
+                nix::RangeDimension dim = array.getDimension(2).asRangeDimension();
+                return QString::fromStdString(nix::util::numToStr(dim[section]));
+            } else if (array.getDimension(2).dimensionType() == nix::DimensionType::Sample) {
+                nix::SampledDimension dim = array.getDimension(2).asSampledDimension();
+                return dim[section];
+            }
+        }
+    } else if (orientation == Qt::Orientation::Vertical) {
+        return "1";
+
+    }
+
+
+    std::cerr << "ping3" << std::endl;
     return "1";
 }
 
