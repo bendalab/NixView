a9aa5357f30261ac0bf1644b64d40322169a7555
Jan Grewe <jan.grewe@g-node.org>
2016-04-21 21:12:23 +0200

    [datatable] control navigation buttons

--- views/datatable.cpp
+++ views/datatable.cpp
@@ -10,7 +10,11 @@ DataTable::DataTable(QWidget *parent) :
     ui(new Ui::DataTable), model()
 {
     ui->setupUi(this);
+    ui->navigation_widget->setVisible(false);
+    ui->back_btn->setEnabled(false);
+    ui->next_btn->setEnabled(false);
 }
+
 
 DataTable::~DataTable()
 {
@@ -33,9 +37,41 @@ void DataTable::set_entity(const QModelIndex qml) {
     NixModelItem *item = MainViewWidget::get_current_model()->get_item_from_qml(qml);
     if (strcmp(item->get_nix_qvariant_type().c_str(), NIX_STRING_DATAARRAY) == 0) {
         this->array = item->get_nix_entity<nix::DataArray>();
+        if (array.dataExtent().size() > 2) {
+            ui->navigation_widget->setVisible(true);
+            ui->total_count_label->setText(QString::fromStdString(nix::util::numToStr(array.dataExtent()[2])));
+            ui->current_page->setText(QVariant(1).toString());
+            ui->current_page->setValidator(new QIntValidator(0, (int)array.dataExtent()[2]));
+            ui->next_btn->setEnabled(true);
+        }
         build_model();
         describe();
     }
+}
+
+
+
+void DataTable::next_page() {
+    int curr_page = QVariant(ui->current_page->text()).toInt();
+    curr_page++;
+    ui->current_page->setText(QVariant(curr_page).toString());
+    select_page();
+}
+
+
+void DataTable::previous_page() {
+    int curr_page = QVariant(ui->current_page->text()).toInt();
+    curr_page--;
+    ui->current_page->setText(QVariant(curr_page).toString());
+    select_page();
+}
+
+
+void DataTable::select_page() {
+    int curr_page = QVariant(ui->current_page->text()).toInt();
+    ui->next_btn->setEnabled(curr_page < array.dataExtent()[2]);
+    ui->back_btn->setEnabled(curr_page > 1);
+    build_model(curr_page - 1);
 }
 
 
@@ -55,12 +91,12 @@ EntityDescriptor DataTable::basic_description(const std::string &name, const std
 }
 
 
-void DataTable::build_model() {
+void DataTable::build_model(int page) {
     if (this->model != nullptr)
         delete model;
 
     model = new NixArrayTableModel(this);
-    model->set_source(array);
+    model->set_source(array, page);
     ui->table->setModel(model);
     ui->table->setSelectionMode(QAbstractItemView::ContiguousSelection);
 }
