bdc3c566005178bf4eacda117908c923eda9887b
Jan Grewe <jan.grewe@g-node.org>
2016-05-05 20:02:35 +0200

    [optionsdialg] new class that shows the options in a modal dialog

---
+++ dialogs/optionsdialog.cpp
@@ -0,0 +1,14 @@
+#include "optionsdialog.h"
+#include "ui_optionsdialog.h"
+
+OptionsDialog::OptionsDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::OptionsDialog)
+{
+    ui->setupUi(this);
+}
+
+OptionsDialog::~OptionsDialog()
+{
+    delete ui;
+}
