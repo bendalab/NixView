a2d557ababf3cfbcecb84f20a0e209ac069dd65a
Jan Grewe <jan.grewe@g-node.org>
2016-10-25 16:18:20 +0200

    [search] enable searching for all of one type if no search term is given

--- searchform.cpp
+++ searchform.cpp
@@ -24,35 +24,38 @@ SearchForm::~SearchForm() {
 void SearchForm::go() {
     this->results.clear();
     if (this->nix_file != nix::none) {
-        if (ui->termEdit->text().size() == 0) {
-            return;
-        }
         bool exact = ui->exactCheckBox->isChecked();
         bool case_sensitive = ui->caseSensitivityCheckBox->isChecked();
         std::string term = ui->termEdit->text().toStdString();
-
         switch (ui->typeComboBox->currentIndex()) {
         case 0: { // looking for data arrays
             std::vector<nix::DataArray> arrays;
-            if (ui->fieldComboBox->currentIndex() == 0) {
+            if (term.length() == 0){
                 for (nix::Block b : this->nix_file.blocks()){
-                    std::vector<nix::DataArray> temp = b.dataArrays(NameSearch<nix::DataArray>(term, case_sensitive, exact));
+                    std::vector<nix::DataArray> temp = b.dataArrays();
                     arrays.insert(arrays.end(), temp.begin(), temp.end());
                 }
-            } else if (ui->fieldComboBox->currentIndex() == 1) {
-                for (nix::Block b : this->nix_file.blocks()){
-                    std::vector<nix::DataArray> temp = b.dataArrays(TypeSearch<nix::DataArray>(term, case_sensitive, exact));
-                    arrays.insert(arrays.end(), temp.begin(), temp.end());
-                }
-            } else if (ui->fieldComboBox->currentIndex() == 2) {
-                for (nix::Block b : this->nix_file.blocks()){
-                    std::vector<nix::DataArray> temp = b.dataArrays(nix::util::IdFilter<nix::DataArray>(term));
-                    arrays.insert(arrays.end(), temp.begin(), temp.end());
-                }
-            } else if (ui->fieldComboBox->currentIndex() == 3) {
-                for (nix::Block b : this->nix_file.blocks()){
-                    std::vector<nix::DataArray> temp = b.dataArrays(DefinitionSearch<nix::DataArray>(term, case_sensitive, exact));
-                    arrays.insert(arrays.end(), temp.begin(), temp.end());
+            } else {
+                if (ui->fieldComboBox->currentIndex() == 0) {
+                    for (nix::Block b : this->nix_file.blocks()){
+                        std::vector<nix::DataArray> temp = b.dataArrays(NameSearch<nix::DataArray>(term, case_sensitive, exact));
+                        arrays.insert(arrays.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->fieldComboBox->currentIndex() == 1) {
+                    for (nix::Block b : this->nix_file.blocks()){
+                        std::vector<nix::DataArray> temp = b.dataArrays(TypeSearch<nix::DataArray>(term, case_sensitive, exact));
+                        arrays.insert(arrays.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->fieldComboBox->currentIndex() == 2) {
+                    for (nix::Block b : this->nix_file.blocks()){
+                        std::vector<nix::DataArray> temp = b.dataArrays(nix::util::IdFilter<nix::DataArray>(term));
+                        arrays.insert(arrays.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->fieldComboBox->currentIndex() == 3) {
+                    for (nix::Block b : this->nix_file.blocks()){
+                        std::vector<nix::DataArray> temp = b.dataArrays(DefinitionSearch<nix::DataArray>(term, case_sensitive, exact));
+                        arrays.insert(arrays.end(), temp.begin(), temp.end());
+                    }
                 }
             }
             for (nix::DataArray da : arrays) {
@@ -62,14 +65,18 @@ void SearchForm::go() {
         }
         case 1: {  // looking for blocks
             std::vector<nix::Block> blocks;
-            if (ui->fieldComboBox->currentIndex() == 0) {
-                blocks = this->nix_file.blocks(NameSearch<nix::Block>(term, case_sensitive, exact));
-            } else if (ui->fieldComboBox->currentIndex() == 1) {
-                blocks = this->nix_file.blocks(TypeSearch<nix::Block>(term, case_sensitive, exact));
-            } else if (ui->fieldComboBox->currentIndex() == 2) {
-                blocks = this->nix_file.blocks(nix::util::IdFilter<nix::Block>(term));
-            } else if (ui->fieldComboBox->currentIndex() == 3) {
-                blocks = this->nix_file.blocks(DefinitionSearch<nix::Block>(term, case_sensitive, exact));
+            if (term.length() == 0){
+                blocks = this->nix_file.blocks();
+            } else {
+                if (ui->fieldComboBox->currentIndex() == 0) {
+                    blocks = this->nix_file.blocks(NameSearch<nix::Block>(term, case_sensitive, exact));
+                } else if (ui->fieldComboBox->currentIndex() == 1) {
+                    blocks = this->nix_file.blocks(TypeSearch<nix::Block>(term, case_sensitive, exact));
+                } else if (ui->fieldComboBox->currentIndex() == 2) {
+                    blocks = this->nix_file.blocks(nix::util::IdFilter<nix::Block>(term));
+                } else if (ui->fieldComboBox->currentIndex() == 3) {
+                    blocks = this->nix_file.blocks(DefinitionSearch<nix::Block>(term, case_sensitive, exact));
+                }
             }
             for (nix::Block b : blocks) {
                 this->results.push_back(QVariant::fromValue(b));
@@ -78,25 +85,32 @@ void SearchForm::go() {
         }
         case 2: { // looking for tags
             std::vector<nix::Tag> tags;
-            if (ui->fieldComboBox->currentIndex() == 0) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::Tag> temp = b.tags(NameSearch<nix::Tag>(term, case_sensitive, exact));
+            if (term.length() == 0) {
+                for (nix::Block b : this->nix_file.blocks()){
+                    std::vector<nix::Tag> temp = b.tags();
                     tags.insert(tags.end(), temp.begin(), temp.end());
                 }
-            } else if (ui->fieldComboBox->currentIndex() == 1) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::Tag> temp = b.tags(TypeSearch<nix::Tag>(term, case_sensitive, exact));
-                    tags.insert(tags.end(), temp.begin(), temp.end());
-                }
-            } else if (ui->fieldComboBox->currentIndex() == 2) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::Tag> temp = b.tags(nix::util::IdFilter<nix::Tag>(term));
-                    tags.insert(tags.end(), temp.begin(), temp.end());
-                }
-            } else if (ui->fieldComboBox->currentIndex() == 3) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::Tag> temp = b.tags(DefinitionSearch<nix::Tag>(term, case_sensitive, exact));
-                    tags.insert(tags.end(), temp.begin(), temp.end());
+            } else {
+                if (ui->fieldComboBox->currentIndex() == 0) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::Tag> temp = b.tags(NameSearch<nix::Tag>(term, case_sensitive, exact));
+                        tags.insert(tags.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->fieldComboBox->currentIndex() == 1) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::Tag> temp = b.tags(TypeSearch<nix::Tag>(term, case_sensitive, exact));
+                        tags.insert(tags.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->fieldComboBox->currentIndex() == 2) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::Tag> temp = b.tags(nix::util::IdFilter<nix::Tag>(term));
+                        tags.insert(tags.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->fieldComboBox->currentIndex() == 3) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::Tag> temp = b.tags(DefinitionSearch<nix::Tag>(term, case_sensitive, exact));
+                        tags.insert(tags.end(), temp.begin(), temp.end());
+                    }
                 }
             }
             for (nix::Tag t : tags) {
@@ -106,25 +120,32 @@ void SearchForm::go() {
         }
         case 3: { // looking for MultiTags
             std::vector<nix::MultiTag> tags;
-            if (ui->fieldComboBox->currentIndex() == 0) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::MultiTag> temp = b.multiTags(NameSearch<nix::MultiTag>(term, case_sensitive, exact));
+            if (term.length() == 0) {
+                for (nix::Block b : this->nix_file.blocks()){
+                    std::vector<nix::MultiTag> temp = b.multiTags();
                     tags.insert(tags.end(), temp.begin(), temp.end());
                 }
-            } else if (ui->fieldComboBox->currentIndex() == 1) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::MultiTag> temp = b.multiTags(TypeSearch<nix::MultiTag>(term, case_sensitive, exact));
-                    tags.insert(tags.end(), temp.begin(), temp.end());
-                }
-            } else if (ui->fieldComboBox->currentIndex() == 2) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::MultiTag> temp = b.multiTags(nix::util::IdFilter<nix::MultiTag>(term));
-                    tags.insert(tags.end(), temp.begin(), temp.end());
-                }
-            } else if (ui->fieldComboBox->currentIndex() == 3) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::MultiTag> temp = b.multiTags(DefinitionSearch<nix::MultiTag>(term, case_sensitive, exact));
-                    tags.insert(tags.end(), temp.begin(), temp.end());
+            } else {
+                if (ui->fieldComboBox->currentIndex() == 0) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::MultiTag> temp = b.multiTags(NameSearch<nix::MultiTag>(term, case_sensitive, exact));
+                        tags.insert(tags.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->fieldComboBox->currentIndex() == 1) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::MultiTag> temp = b.multiTags(TypeSearch<nix::MultiTag>(term, case_sensitive, exact));
+                        tags.insert(tags.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->fieldComboBox->currentIndex() == 2) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::MultiTag> temp = b.multiTags(nix::util::IdFilter<nix::MultiTag>(term));
+                        tags.insert(tags.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->fieldComboBox->currentIndex() == 3) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::MultiTag> temp = b.multiTags(DefinitionSearch<nix::MultiTag>(term, case_sensitive, exact));
+                        tags.insert(tags.end(), temp.begin(), temp.end());
+                    }
                 }
             }
             for (nix::MultiTag t : tags) {
@@ -134,25 +155,32 @@ void SearchForm::go() {
         }
         case 4: { // looking for Groups
             std::vector<nix::Group> groups;
-            if (ui->fieldComboBox->currentIndex() == 0) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::Group> temp = b.groups(NameSearch<nix::Group>(term, case_sensitive, exact));
+            if (term.length() == 0) {
+                for (nix::Block b : this->nix_file.blocks()){
+                    std::vector<nix::Group> temp = b.groups();
                     groups.insert(groups.end(), temp.begin(), temp.end());
                 }
-            } else if (ui->fieldComboBox->currentIndex() == 1) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::Group> temp = b.groups(TypeSearch<nix::Group>(term, case_sensitive, exact));
-                    groups.insert(groups.end(), temp.begin(), temp.end());
-                }
-            } else if (ui->fieldComboBox->currentIndex() == 2) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::Group> temp = b.groups(nix::util::IdFilter<nix::Group>(term));
-                    groups.insert(groups.end(), temp.begin(), temp.end());
-                }
-            } else if (ui->fieldComboBox->currentIndex() == 3) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::Group> temp = b.groups(DefinitionSearch<nix::Group>(term, case_sensitive, exact));
-                    groups.insert(groups.end(), temp.begin(), temp.end());
+            } else {
+                if (ui->fieldComboBox->currentIndex() == 0) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::Group> temp = b.groups(NameSearch<nix::Group>(term, case_sensitive, exact));
+                        groups.insert(groups.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->fieldComboBox->currentIndex() == 1) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::Group> temp = b.groups(TypeSearch<nix::Group>(term, case_sensitive, exact));
+                        groups.insert(groups.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->fieldComboBox->currentIndex() == 2) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::Group> temp = b.groups(nix::util::IdFilter<nix::Group>(term));
+                        groups.insert(groups.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->fieldComboBox->currentIndex() == 3) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::Group> temp = b.groups(DefinitionSearch<nix::Group>(term, case_sensitive, exact));
+                        groups.insert(groups.end(), temp.begin(), temp.end());
+                    }
                 }
             }
             for (nix::Group g : groups) {
@@ -162,25 +190,32 @@ void SearchForm::go() {
         }
         case 5: { // looking for Sources
             std::vector<nix::Source> srcs;
-            if (ui->typeComboBox->currentIndex() == 0) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::Source> temp = b.sources(NameSearch<nix::Source>(term, case_sensitive, exact));
+            if (term.length() == 0) {
+                for (nix::Block b : this->nix_file.blocks()){
+                    std::vector<nix::Source> temp = b.sources();
                     srcs.insert(srcs.end(), temp.begin(), temp.end());
                 }
-            } else if (ui->typeComboBox->currentIndex() == 1) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::Source> temp = b.sources(TypeSearch<nix::Source>(term, case_sensitive, exact));
-                    srcs.insert(srcs.end(), temp.begin(), temp.end());
-                }
-            } else if (ui->typeComboBox->currentIndex() == 2) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::Source> temp = b.sources(nix::util::IdFilter<nix::Source>(term));
-                    srcs.insert(srcs.end(), temp.begin(), temp.end());
-                }
-            } else if (ui->typeComboBox->currentIndex() == 3) {
-                for (nix::Block b : this->nix_file.blocks()) {
-                    std::vector<nix::Source> temp = b.sources(DefinitionSearch<nix::Source>(term, case_sensitive, exact));
-                    srcs.insert(srcs.end(), temp.begin(), temp.end());
+            } else {
+                if (ui->typeComboBox->currentIndex() == 0) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::Source> temp = b.sources(NameSearch<nix::Source>(term, case_sensitive, exact));
+                        srcs.insert(srcs.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->typeComboBox->currentIndex() == 1) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::Source> temp = b.sources(TypeSearch<nix::Source>(term, case_sensitive, exact));
+                        srcs.insert(srcs.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->typeComboBox->currentIndex() == 2) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::Source> temp = b.sources(nix::util::IdFilter<nix::Source>(term));
+                        srcs.insert(srcs.end(), temp.begin(), temp.end());
+                    }
+                } else if (ui->typeComboBox->currentIndex() == 3) {
+                    for (nix::Block b : this->nix_file.blocks()) {
+                        std::vector<nix::Source> temp = b.sources(DefinitionSearch<nix::Source>(term, case_sensitive, exact));
+                        srcs.insert(srcs.end(), temp.begin(), temp.end());
+                    }
                 }
             }
             for (nix::Source s : srcs) {
