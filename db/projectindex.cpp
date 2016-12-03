0b6929df73710b6d08f684ef4de1bfeac8bd977a
Jan Grewe <jan.grewe@g-node.org>
2016-12-03 12:39:09 +0100

    [project index] basic class for managing project files

--- db/projectindex.cpp
+++ db/projectindex.cpp
@@ -1,6 +1,75 @@
 #include "projectindex.hpp"
+#include <QFile>
+#include <QDir>
+#include <iostream>
+#include <QSqlQuery>
+#include <QVariant>
 
-ProjectIndex::ProjectIndex()
-{
+ProjectIndex::ProjectIndex(const QString &path):path(path){
+    QFile f(path);
+    if (!f.exists()) {
+        create_project_index(path);
+    } else {
+        QSqlDatabase index_db = QSqlDatabase::addDatabase("QSQLITE",  path);
+        index_db.setDatabaseName(path);
+        if (!index_db.open()) {
+            std::cerr << "failed to open database!\n";
+        }
+    }
+}
+
+
+bool ProjectIndex::add_file(const QString &file_path) {
+    QFile f(file_path);
+    QDir d(file_path);
+    QString name = d.dirName();
+    if (!f.exists()) {
+        return false;
+    }
+    QSqlDatabase db = QSqlDatabase::database(path);
+    db.open();
+    QSqlQuery q(db);
+    q.prepare("INSERT INTO files (name, path) VALUES (:name, :path)");
+    q.bindValue(":name", QVariant(name));
+    q.bindValue(":path", QVariant(file_path));
+    db.close();
+    index_file(file_path);
+    return true;
+}
+
+
+void ProjectIndex::index_file(const QString &file_path) {
+    QSqlDatabase db = QSqlDatabase::database(this->path);
+    db.open();
+
+
+
+    db.close();
 
 }
+
+
+bool ProjectIndex::create_project_index(const QString &path) {
+    bool success = false;
+    QFile f(path);
+    if (f.exists()) {
+        success = true;
+    } else {
+        std::cerr << "create project index" << std::endl;
+        QSqlDatabase index_db = QSqlDatabase::addDatabase("QSQLITE",  path);
+        index_db.setDatabaseName(path);
+        if (!index_db.open()) {
+            std::cerr << "failed to open database!\n";
+        }
+        QSqlQuery q(QSqlDatabase::database(path));
+        success = q.exec(QLatin1String("create table files(id integer primary key, name varchar, path varchar)"));
+        if(success)
+            success = q.exec(QLatin1String("create table data_index(id integer primary key, file_id integer, entity_name varchar, entity_type varchar)"));
+        if(success)
+            q.exec(QLatin1String("ALTER TABLE data_index ADD FOREIGN KEY (file_id) REFERENCES files (id)"));
+        index_db.close();
+    }
+
+    return success;
+}
+
--- db/projectindex.hpp
+++ db/projectindex.hpp
@@ -1,11 +1,21 @@
 #ifndef PROJECTINDEX_H
 #define PROJECTINDEX_H
+#include <QString>
+#include <QSql>
 
+class ProjectIndex {
 
-class ProjectIndex
-{
+private:
+    QString path;
+
+    void index_file(const QString &file_path);
+
 public:
-    ProjectIndex();
+    ProjectIndex(const QString &path);
+
+    bool add_file(const QString &file_path);
+
+    static bool create_project_index(const QString &path);
 };
 
 #endif // PROJECTINDEX_H
