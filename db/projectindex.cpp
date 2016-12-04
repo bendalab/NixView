f5446d6f95571fc7c01df2babf6e7c2877142ca9
Jan Grewe <jan.grewe@g-node.org>
2016-12-04 17:42:12 +0100

    [ProjectIndex] add methods to get file list and path of a file

--- db/projectindex.cpp
+++ db/projectindex.cpp
@@ -24,7 +24,41 @@ ProjectIndex::ProjectIndex(const QString &path)
 }
 
 
+QStringList ProjectIndex::get_file_list() {
+    QStringList list;
+    QSqlDatabase db = QSqlDatabase::database(path);
+    if (!db.open())
+        return list;
+    QSqlQuery q(db);
+    q.prepare("SELECT name FROM files");
+    if(!q.exec())
+        return list;
+    while (q.next()) {
+        list.append(q.value(0).toString());
+    }
+    return list;
+}
+
+
+QString ProjectIndex::get_file_path(const QString &file_name) {
+    QString file_path;
+    QSqlDatabase db = QSqlDatabase::database(path);
+    if (!db.open())
+        return file_path;
+    QSqlQuery q(db);
+    q.prepare("SELECT path FROM files WHERE name = (:name)");
+    q.bindValue(":name", file_name);
+    if(!q.exec())
+        return file_path;
+    if (q.next()) {
+        return file_path = q.value(0).toString();
+    }
+    return file_path;
+}
+
+
 bool ProjectIndex::add_file(const QString &file_path) {
+    std::cerr << "add file:  " << file_path.toStdString() << std::endl;
     QFile f(file_path);
     QDir d(file_path);
     QString name = d.dirName();
@@ -32,11 +66,14 @@ bool ProjectIndex::add_file(const QString &file_path) {
         return false;
     }
     QSqlDatabase db = QSqlDatabase::database(path);
-    db.open();
+    std::cerr << db.open() << std::endl;
     QSqlQuery q(db);
     q.prepare("INSERT INTO files (name, path) VALUES (:name, :path)");
     q.bindValue(":name", QVariant(name));
     q.bindValue(":path", QVariant(file_path));
+    if (!q.exec()){
+        std::cerr << "Something went wrong when adding file: " << file_path.toStdString() << std::endl;
+    }
     db.close();
     index_file(file_path);
     return true;
--- db/projectindex.hpp
+++ db/projectindex.hpp
@@ -13,6 +13,8 @@ private:
 public:
     ProjectIndex(const QString &path);
 
+    QStringList get_file_list();
+    QString get_file_path(const QString &file_name);
     bool add_file(const QString &file_path);
 
     static bool create_project_index(const QString &path);
