ade4fae844ec1a8614e639aefb00178d6dc49e6d
Jan Grewe <jan.grewe@g-node.org>
2017-05-07 17:36:05 +0200

    [cmake] change some bundle settings

--- cmake/QtCommon.cmake
+++ cmake/QtCommon.cmake
@@ -1,76 +1,71 @@
-MESSAGE("====== QTCommon ======")
+MESSAGE("=> setup Qt bundle options ...")
 
 macro(fix_project_version)
-if (NOT PROJECT_VERSION_PATCH)
+  if (NOT PROJECT_VERSION_PATCH)
     set(PROJECT_VERSION_PATCH 0)
-endif()
+  endif()
 
-if (NOT PROJECT_VERSION_TWEAK)
+  if (NOT PROJECT_VERSION_TWEAK)
     set(PROJECT_VERSION_TWEAK 0)
-endif()
+  endif()
 endmacro()
 
 macro(add_project_meta FILES_TO_INCLUDE)
-if (NOT RESOURCE_FOLDER)
+  if (NOT RESOURCE_FOLDER)
     set(RESOURCE_FOLDER images)
-endif()
+  endif()
 
-if (NOT ICON_NAME)
+  if (NOT ICON_NAME)
     set(ICON_NAME nixview)
-endif()
+  endif()
 
-if (APPLE)
+  if (APPLE)
     set(ICON_FILE ${RESOURCE_FOLDER}/${ICON_NAME}.icns)
-elseif (WIN32)
+  elseif (WIN32)
     set(ICON_FILE ${RESOURCE_FOLDER}/${ICON_NAME}.ico)
-endif()
+  endif()
 
-if (APPLE)
+  if (APPLE)
     set_source_files_properties(${ICON_FILE} PROPERTIES MACOSX_PACKAGE_LOCATION Resources)
-    MESSAGE("====== init bundle ======")
     # Identify MacOS bundle
     set(MACOSX_BUNDLE_BUNDLE_NAME ${PROJECT_NAME})
-    MESSAGE(" ${PROJECT_NAME}")
     set(MACOSX_BUNDLE_BUNDLE_VERSION ${PROJECT_VERSION})
-    MESSAGE(" ${PROJECT_NAME}")
     set(MACOSX_BUNDLE_LONG_VERSION_STRING ${PROJECT_VERSION})
     set(MACOSX_BUNDLE_SHORT_VERSION_STRING "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}")
     set(MACOSX_BUNDLE_COPYRIGHT ${COPYRIGHT})
     set(MACOSX_BUNDLE_GUI_IDENTIFIER ${IDENTIFIER})
-    MESSAGE(" ${IDENTIFIER}")
     set(MACOSX_BUNDLE_ICON_FILE ${ICON_NAME})
-    MESSAGE(" ${ICON_FILE}")
-endif()
+  endif()
 
-if (APPLE)
+  if (APPLE)
     set(${FILES_TO_INCLUDE} ${ICON_FILE})
-elseif (WIN32)
+  elseif (WIN32)
     set(${FILES_TO_INCLUDE} ${RES_FILES})
-endif()
+  endif()
 endmacro()
 
 macro(init_os_bundle)
-if (APPLE)
-   set(OS_BUNDLE MACOSX_BUNDLE)
-elseif (WIN32)
+  if (APPLE)
+    set(OS_BUNDLE MACOSX_BUNDLE)
+  elseif (WIN32)
     set(OS_BUNDLE WIN32)
-endif()
+  endif()
 endmacro()
 
 macro(fix_win_compiler)
-if (MSVC)
+  if (MSVC)
     set_target_properties(${PROJECT_NAME} PROPERTIES
-        WIN32_EXECUTABLE YES
-        LINK_FLAGS "/ENTRY:mainCRTStartup"
-    )
-endif()
+      WIN32_EXECUTABLE YES
+      LINK_FLAGS "/ENTRY:mainCRTStartup"
+      )
+  endif()
 endmacro()
 
 macro(init_qt)
   # Let's do the CMake job for us
-	set(CMAKE_AUTOMOC ON) # For meta object compiler
-	set(CMAKE_AUTORCC ON) # Resource files
-	set(CMAKE_AUTOUIC ON) # UI files
+  set(CMAKE_AUTOMOC ON) # For meta object compiler
+  set(CMAKE_AUTORCC ON) # Resource files
+  set(CMAKE_AUTOUIC ON) # UI files
 endmacro()
 
 init_os_bundle()
