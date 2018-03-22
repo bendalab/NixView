06ca33d174d02f45f37746c8bd037ed582e99c96
Alexander Ott <a.ott@student.uni-tuebingen.de>
2018-03-22 09:28:13 +0100

    rebuilt for loading certein indicies of 2D arrays + rename extent + added debug try catch

--- utils/loadthread.cpp
+++ utils/loadthread.cpp
@@ -22,8 +22,9 @@ LoadThread::~LoadThread() {
 
 
 void LoadThread::run() {
-
+    try {
     while(! abort) {
+
         mutex.lock();
         if(restart) {
             mutex.unlock();
@@ -33,66 +34,38 @@ void LoadThread::run() {
 
         nix::DataArray array = this->array;
         nix::NDSize start = this->start;
-        nix::NDSize extend = this->extend;
+        nix::NDSize extent = this->extent;
         unsigned int chunksize = this->chunksize;
-        int index = this->index;
+        int graphIndex = this->graphIndex;
         mutex.unlock();
 
-        unsigned int dataLength = 1;
-        int readDim =0;
-        unsigned int offset = start[0];
-        for (unsigned int i=0; i<extend.size(); i++) {
-            if(extend[i] > 1) // extend has to define 1d data (all entries 1 exept for one)
-                dataLength = extend[i];
-                readDim = static_cast<int>(i);
-                offset = start[i];
+        int dimCount;
+        try {
+        dimCount = array.dimensionCount();
+        } catch(...) {
+            std::cerr << "ARRAY DIMCOUNT IS A PROBLEM" << std::endl;
+            dimCount = 1;
         }
 
-        int totalChunks;
-        if( dataLength / chunksize == static_cast<double>(dataLength) / chunksize) {
-            totalChunks = dataLength / chunksize;
-        } else {
-            totalChunks = dataLength / chunksize + 1;
-        }
-
-        extend[readDim] = chunksize;
-        QVector<double> loadedData;
-        QVector<double> chunkdata(chunksize);
-        bool brokenData = false;
-        for (int i=0; i<totalChunks; i++) {
-            mutex.lock();
-            if(restart) {
-                brokenData = true;
-                mutex.unlock();
-                break;
-            }
-            mutex.unlock();
-
-            emit(progress(static_cast<double>(i)/totalChunks, index)); //starts with 0 ends with one step below 1
-
-            if(i == totalChunks-1) {
-                extend[readDim] = (dataLength - (totalChunks-1) * chunksize);
-                chunkdata.resize((dataLength - (totalChunks-1) * chunksize));
-            }
-            start[readDim] = offset + i * chunksize;
-
-            array.getData(array.dataType(),chunkdata.data(),extend, start);
-
-            loadedData.append(chunkdata);
-            }
-
-        if(! brokenData) {
-            QVector<double> axis(0);
-
+        if(dimCount == 1) {
             try {
-                getAxis(array, axis, dataLength, offset, readDim +1);
-            } catch(...) {
-                //Throws exceptions without wait time at the start.
-                //std::cerr << "------------getAxis is the problem!" << std::endl;
-                continue;
+            load1D(array, start, extent, chunksize, graphIndex);
+            } catch (...) {
+                std::cerr << "SOMEWHERE IN LOAD 1D" << std::endl;
             }
 
-            emit dataReady(loadedData, axis, index);
+
+        } else if(dimCount == 2) {
+            std::cerr << "its not a two D array" << std::endl;
+            mutex.lock();
+            unsigned int xDim = this->xDim;
+            std::vector<int> index2D = this->index2D;
+            mutex.unlock();
+            try {
+            load2D(array, start, extent, xDim, index2D, chunksize, graphIndex);
+            } catch(...) {
+                std::cerr << "WHY THE FUCK IN LOAD2D()" << std::endl;
+            }
         }
 
         mutex.lock();
@@ -102,9 +75,143 @@ void LoadThread::run() {
             restart = false;
         }
         mutex.unlock();
-
+    }
+    } catch(...) {
+        std::cerr << "SOMEWHERE IN RUN() where it wasn't caught GREAT" << std::endl;
     }
 }
+
+
+void LoadThread::load1D(nix::DataArray array, nix::NDSize start, nix::NDSize extent, unsigned int chunksize, int graphIndex) {
+
+    int dataLength = extent[0];
+    unsigned int offset = start[0];
+    int totalChunks = (dataLength / chunksize) + 1;
+
+    if( dataLength / chunksize == static_cast<double>(dataLength) / chunksize) {
+        totalChunks = dataLength / chunksize;
+    }
+
+    extent[0] = chunksize;
+    QVector<double> loadedData;
+    QVector<double> chunkdata(chunksize);
+
+    bool brokenData = false;
+    for (int i=0; i<totalChunks; i++) {
+
+        mutex.lock();
+        if(restart) {
+            brokenData = true;
+            mutex.unlock();
+            break;
+        }
+        mutex.unlock();
+
+        emit(progress(static_cast<double>(i)/totalChunks, graphIndex)); //starts with 0 ends with one step below 1
+
+        if(i == totalChunks-1) {
+            extent[0] = (dataLength - (totalChunks-1) * chunksize);
+            chunkdata.resize((dataLength - (totalChunks-1) * chunksize));
+        }
+        start[0] = offset + i * chunksize;
+
+        try {
+        array.getData(array.dataType(),chunkdata.data(),extent, start);
+        } catch (...) {
+            std::cerr << "GET DATA - is the culprit ***********" << std::endl;
+        }
+
+        loadedData.append(chunkdata);
+    }
+
+    if(! brokenData) {
+        QVector<double> axis(0);
+
+        try {
+            getAxis(array, axis, dataLength, offset, xDim);
+        } catch(...) {
+            std::cerr << "LoadThread::load1D::getAxis() threw an error." << std::endl;
+        }
+        emit dataReady(loadedData, axis, graphIndex);
+    }
+}
+
+void LoadThread::load2D(nix::DataArray array, nix::NDSize start, nix::NDSize extent,  unsigned int xDim, std::vector<int> index2D, unsigned int chunksize, int graphIndex) {
+
+    unsigned int xDimIndex = xDim-1;
+    unsigned int dataLength = extent[xDimIndex];
+    unsigned int offset = start[xDimIndex];
+
+    int totalChunks;
+    if( dataLength / chunksize == static_cast<double>(dataLength) / chunksize) {
+        totalChunks = dataLength / chunksize;
+    } else {
+        totalChunks = dataLength / chunksize + 1;
+    }
+
+    extent[xDimIndex] = chunksize;
+    QVector<double> loadedData;
+    QVector<double> chunkdata(chunksize);
+
+    //if index2D is empty do all.
+    if(index2D.size() == 0) {
+        index2D.resize(array.dataExtent()[1-xDimIndex]);
+        for(unsigned int i=0; i< array.dataExtent()[1-xDimIndex]; i++) {
+            index2D[i] = static_cast<int>(i);
+        }
+    }
+
+    //iterate over given indices
+    for(unsigned int j=0; j<index2D.size(); j++) {
+        bool brokenData = false;
+        start[1-xDimIndex] = index2D[j];
+
+        mutex.lock();
+        if(restart) {
+            brokenData = true;
+            mutex.unlock();
+            break;
+        }
+        mutex.unlock();
+
+        for (int i=0; i<totalChunks; i++) {
+
+            mutex.lock();
+            if(restart) {
+                brokenData = true;
+                mutex.unlock();
+                break;
+            }
+            mutex.unlock();
+
+            emit(progress(static_cast<double>(i)*j / (totalChunks*index2D.size()), graphIndex)); //starts with 0 ends with one step below 1
+
+
+            if(i == totalChunks-1) {
+                extent[xDimIndex] = (dataLength - (totalChunks-1) * chunksize);
+                chunkdata.resize((dataLength - (totalChunks-1) * chunksize));
+            }
+            start[xDimIndex] = offset + i * chunksize;
+
+            array.getData(array.dataType(),chunkdata.data(),extent, start);
+
+            loadedData.append(chunkdata);
+            }
+
+        if(! brokenData) {
+            QVector<double> axis(0);
+
+            try {
+                getAxis(array, axis, dataLength, offset, xDim);
+                emit dataReady(loadedData, axis, graphIndex + index2D[j]);
+            } catch(...) {
+                std::cerr << "LoadThread::load2D::getAxis() threw an error." << std::endl;
+            }
+
+        }
+    }
+}
+
 
 
 void LoadThread::getAxis(const nix::DataArray &array, QVector<double> &axis, unsigned int count, unsigned int offset, int xDim) {
@@ -122,8 +229,10 @@ void LoadThread::getAxis(const nix::DataArray &array, QVector<double> &axis, uns
 }
 
 
-void LoadThread::setVariables(const nix::DataArray &array, nix::NDSize start, nix::NDSize extend, int index) {
-    if(! testInput(array, start, extend)) {
+void LoadThread::setVariables(const nix::DataArray &array, nix::NDSize start, nix::NDSize extent, std::vector<int> index2D, unsigned int xDim, int graphIndex) {
+    try {
+
+    if(! testInput(array, start, extent)) {
         std::cerr << "LoadThread::setVariables(): Input not correct." << std::endl;
         return;
     }
@@ -132,8 +241,10 @@ void LoadThread::setVariables(const nix::DataArray &array, nix::NDSize start, ni
 
     this->array = array;
     this->start = start;
-    this->extend = extend;
-    this->index = index;
+    this->extent = extent;
+    this->graphIndex = graphIndex;
+    this->index2D = index2D;
+    this->xDim = xDim;
 
     if(! isRunning()) {
         QThread::start(LowPriority);
@@ -141,6 +252,33 @@ void LoadThread::setVariables(const nix::DataArray &array, nix::NDSize start, ni
         this->restart = true;
         condition.wakeOne();
     }
+
+    } catch(...) {
+        std::cerr << "ERROR IN SET VARIABLES()" << std::endl;
+    }
+}
+
+void LoadThread::setVariables1D(const nix::DataArray &array, nix::NDSize start, nix::NDSize extent, int graphIndex) {
+
+    if(array.dimensionCount() != 1) {
+        std::cerr << "LoadThread::setVariables1D() given array has more than 1 dimension." << std::endl;
+    }
+
+    QMutexLocker locker(&mutex);
+
+    this->array = array;
+    this->start = start;
+    this->extent = extent;
+    this->graphIndex = graphIndex;
+
+    if(! isRunning()) {
+        QThread::start(LowPriority);
+    } else {
+        this->restart = true;
+        condition.wakeOne();
+    }
+
+
 }
 
 void LoadThread::setChuncksize(unsigned int size) {
@@ -155,23 +293,24 @@ void LoadThread::setChuncksize(unsigned int size) {
 }
 
 
-bool LoadThread::testInput(const nix::DataArray &array, nix::NDSize start, nix::NDSize extend) {
+bool LoadThread::testInput(const nix::DataArray &array, nix::NDSize start, nix::NDSize extent) {
+    try {
     nix::NDSize size = array.dataExtent();
-    if( ! (size.size() == start.size() && size.size() == extend.size())) {
-        std::cerr << "DataThread::testInput(): start and/or extend don't have the same dimensionality as the array." << std::endl;
+    if( ! (size.size() == start.size() && size.size() == extent.size())) {
+        std::cerr << "DataThread::testInput(): start and/or extent don't have the same dimensionality as the array." << std::endl;
         return false;
     }
 
     bool Dataload1d = false;
     for(uint i=0; i<size.size(); i++) {
-        if(size[i] < start[i]+extend[i]) {
-            std::cerr << "DataThread::testInput(): start + extend bigger than length of array in dimension " << i << std::endl;
+        if(size[i] < start[i]+extent[i]) {
+            std::cerr << "DataThread::testInput(): start + extent bigger than length of array in dimension " << i << std::endl;
             return false;
         }
 
-        if(extend[i] > 1) { // extend has to define 1d data (all 1 exept for one entry)
+        if(extent[i] > 1) { // extent has to define 1d data (all 1 exept for one entry)
             if (Dataload1d) {
-                std::cerr << "DataThread::testInput(): extend defines data that is more than one dimensional." << std::endl;
+                std::cerr << "DataThread::testInput(): extent defines data that is more than one dimensional." << std::endl;
                 return false;
             } else {
                 Dataload1d = true;
@@ -183,4 +322,8 @@ bool LoadThread::testInput(const nix::DataArray &array, nix::NDSize start, nix::
     }
 
     return true;
+    }catch(...) {
+        std::cerr << "ERROR IN TEST INPUT()" << std::endl;
+        return false;
+    }
 }
--- utils/loadthread.h
+++ utils/loadthread.h
@@ -12,16 +12,19 @@ class LoadThread: public QThread
     Q_OBJECT
 
 public:
-    LoadThread(QObject *parent = 0, unsigned int chunksize =1000000);
+    LoadThread(QObject *parent = 0, unsigned int chunksize=1000000);
     ~LoadThread();
 
     void run() override;
-    void setVariables(const nix::DataArray &array, nix::NDSize start, nix::NDSize extend, int index);
+    void setVariables(const nix::DataArray &array, nix::NDSize start, nix::NDSize extent, std::vector<int> index2D, unsigned int xDim, int graphIndex);
+    void setVariables1D(const nix::DataArray &array, nix::NDSize start, nix::NDSize extent, int graphIndex);
     void setChuncksize(unsigned int size);
 
 private:
     void getAxis(const nix::DataArray &array, QVector<double> &axis, unsigned int count, unsigned int offset, int xDim);
-    bool testInput(const nix::DataArray &array, nix::NDSize start, nix::NDSize extend);
+    bool testInput(const nix::DataArray &array, nix::NDSize start, nix::NDSize extent);
+    void load1D(nix::DataArray array, nix::NDSize start, nix::NDSize extent, unsigned int chunksize, int graphIndex);
+    void load2D(nix::DataArray array, nix::NDSize start, nix::NDSize extent,  unsigned int xDim, std::vector<int> index2D, unsigned int chunksize, int graphIndex);
 
 signals:
     void dataReady(const QVector<double> &data, const QVector<double> &axis, int index);
@@ -32,12 +35,13 @@ private:
     QWaitCondition condition;
     nix::DataArray array;
     nix::NDSize start;
-    nix::NDSize extend;
+    nix::NDSize extent;
     unsigned int chunksize;
-    int index;
+    unsigned int xDim;
+    std::vector<int> index2D;
+    int graphIndex;
     bool abort;
     bool restart;
-
 };
 
 #endif // LOADTHREAD_H
